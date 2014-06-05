4927e72b80c679ba91bbe8df4e61fc8b8fab914c
kishoreanantatmula <anantha6445@gmail.com>
2014-06-05 15:23:51 +0530

    Commiting the comment
    Comitting the Extra parameter

--- Collections/src/test.java
+++ Collections/src/test.java
@@ -10,12 +10,17 @@ public class test {
 		
 		
 	ArrayList a = new ArrayList();
-		
+	
+	
+	// Checking if everyhitng is added
 	a.add(10);
 	a.add(20);
 	a.add(30);
 	a.add(40);
 	a.add(50);
+	a.add(60);
+	a.add(70);
+	
 	
 	System.out.println(a);	
 	
