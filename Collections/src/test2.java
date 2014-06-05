084e79e18a5c4b87764c600ca19b9750be1f39ba
kishoreanantatmula <anantha6445@gmail.com>
2014-06-05 19:22:07 +0530

    Added new  case

---
+++ Collections/src/test2.java
@@ -0,0 +1,31 @@
+
+import java.util.*;
+
+public class test2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		
+		LinkedList l = new LinkedList();
+		
+		l.add(500);
+		l.add(1500);
+		l.add(2500);
+		l.add(3500);
+		l.add(4500);
+		l.add(5500);
+		l.add(6500);
+		
+		System.out.println(l);
+		
+		
+		
+		
+
+	}
+
+}
