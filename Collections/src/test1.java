abe435fc63fd8eb9c4f0b40ca5e73610d6168b8b
kishoreanantatmula <anantha6445@gmail.com>
2014-06-05 16:38:39 +0530

    cimmit
    Commit

---
+++ Collections/src/test1.java
@@ -0,0 +1,21 @@
+
+
+import java.util.*;
+
+public class test1 {
+
+	public static void main(String args[])
+	{
+		
+		
+		HashMap h = new HashMap();
+		
+		h.put("Ravi", "GTech");
+		h.put("Vinod", "Altimetrik");
+		h.put("Bupesh", "Infosys");
+		
+		System.out.println(h);
+		
+	}
+	
+}
