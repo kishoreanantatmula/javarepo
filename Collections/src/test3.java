7f828708069c3cd5c37388df9f2f9456d40b99f6
kishoreanantatmula <anantha6445@gmail.com>
2014-06-05 20:12:31 +0530

    Addin nre case

---
+++ Collections/src/test3.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+public class test3 {
+	
+	public static void main(String[] args) {
+		
+		
+		TreeSet t = new TreeSet();
+		
+		t.add(400);
+		t.add(300);
+		t.add(450);
+		t.add(600);
+		t.add(750);
+		t.add(425);
+		
+		System.out.println(t);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
