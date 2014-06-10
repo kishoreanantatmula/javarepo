62be015a694d0e6f9674f0cbc01cacae76172b45
kishoreanantatmula <anantha6445@gmail.com>
2014-06-10 14:44:48 +0530

    Commitng test4
    Test 4 commit

---
+++ Collections/src/batchdemo.java
@@ -0,0 +1,16 @@
+
+public class batchdemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		//System.out.println(System.getProperty("batch"));
+		
+		System.out.println(System.getProperty("Ravi"));
+
+	}
+
+}
---
+++ Collections/src/test4.java
@@ -0,0 +1,12 @@
+
+public class test4 {
+	
+	
+	public static void main(String args[])
+	{
+		
+		
+		System.out.println("This is test 4");
+	}
+
+}
